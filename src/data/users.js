f14864f56406dd302c7ed22e6ce0dcbadf4b776f
Paweł Stępień <finestedm@gmail.com>
2022-12-07 18:23:26 +0100

    users sorted alphabetically

--- src/data/users.js
+++ src/data/users.js
@@ -1,7 +1,7 @@
 import KN from '../images/avatars/KN.jfif'
 import RP from '../images/avatars/RP.jfif'
 
-export const users = [
+const users = [
     { value: 'Marcin Rączkowski', label: 'Marcin Rączkowski' },
     { value: 'Rafał Popielarski', label: 'Rafał Popielarski', photo: RP },
     { value: 'Michał Tupacz', label: 'Michał Tupacz' },
@@ -12,3 +12,13 @@ export const users = [
     { value: 'Krystian Nowak', label: 'Krystian Nowak', photo: KN },
     { value: 'Bartosz Marszula', label: 'Bartosz Marszula' },
 ]
+
+//sorting of the users alphabetically by last name
+
+users.sort(function(a, b){
+    if(a.value.substring(a.value.indexOf(' ')) < b.value.substring(b.value.indexOf(' '))) { return -1; }
+    if(a.value.substring(a.value.indexOf(' ')) > b.value.substring(b.value.indexOf(' '))) { return 1; }
+    return 0;
+})
+
+export { users }
