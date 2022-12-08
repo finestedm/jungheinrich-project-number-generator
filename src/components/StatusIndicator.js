2dbfd53f4725cd4e6c28b74c6b9b9f5f66d380c2
Paweł Stępień <finestedm@gmail.com>
2022-12-08 23:02:28 +0100

    status indicator added

--- src/components/StatusIndicator.js
+++ src/components/StatusIndicator.js
@@ -2,7 +2,21 @@ import React from "react";
 
 export default function StatusIndicator({status}) {
     const statusClass = `status-${status}`;
+    const statusDescription = (status) => {
+        switch (status) {
+            case 0:
+                return 'ofertowany';
+            case 1:
+                return 'zamówiony';
+            case 2:
+                return 'przegrany';
+            case 3:
+                return 'zamknięty';
+            default:
+                break;
+        }
+    }
     return (
-        <span className={statusClass}>{status}</span>
+        <span className={statusClass}><div className="d-none d-lg-inline">{statusDescription(status)}</div></span>
     )
 }
