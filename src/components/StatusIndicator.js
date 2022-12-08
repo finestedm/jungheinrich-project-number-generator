4a038f7b99b4f7d96e4c3bbd5a0561d939462b73
Paweł Stępień <finestedm@gmail.com>
2022-12-08 23:19:04 +0100

    status incicator icons in smaller width screens

--- src/components/StatusIndicator.js
+++ src/components/StatusIndicator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RxCross2, RxClock, RxCrumpledPaper, RxCheckCircled, RxArchive, RxLapTimer } from 'react-icons/rx';
 
 export default function StatusIndicator({status}) {
     const statusClass = `status-${status}`;
@@ -16,7 +17,30 @@ export default function StatusIndicator({status}) {
                 break;
         }
     }
+
+    const statusIcon = (status) => {
+        switch (status) {
+            case 0:
+                return <RxLapTimer />;
+            case 1:
+                return <RxCheckCircled />;
+            case 2:
+                return <RxCrumpledPaper />;
+            case 3:
+                return <RxArchive />;
+            default:
+                break;
+        }
+    }
+
     return (
-        <span className={statusClass}><div className="d-none d-lg-inline">{statusDescription(status)}</div></span>
+        <span className={statusClass}>
+            <div className="d-none d-lg-inline">
+                {statusDescription(status)}
+            </div>
+            <div className="d-inline d-lg-none">
+                {statusIcon(status)}
+            </div>
+        </span>
     )
 }
