51399fa59ccee7ce6f203ac220542054f1e9ecc1
Paweł Stępień <finestedm@gmail.com>
2022-12-13 14:11:45 +0100

    header style fixed

--- src/Pages/Archive.js
+++ src/Pages/Archive.js
@@ -51,12 +51,12 @@ export default function Archive() {
     return (
         <Container className='main my-5'>
             <div className='table-container px-2'>
-                <Row className='d-flex flex-column flex-sm-row align-items-center justify-content-center search-container py-4 px-2 gap-3'>
+                <Row className='d-flex flex-column flex-sm-row justify-content-center search-container py-4 px-2 gap-3'>
                     <Col className='col-auto'>
-                        <h2 className='m-0'>Projekty</h2>
+                        <h2 className='mb-4'>Projekty</h2>
+                        <NewProjectCounter className='align' postsThisDay={postsThisDay} />
                     </Col>
-                    <NewProjectCounter postsThisDay={postsThisDay} />
-                    <Col  className='ms-md-auto col-auto main--search-bar'>
+                    <Col  className='ms-md-auto col-auto main--search-bar mt-auto'>
                         <SearchBar searchedPhrase={searchedPhrase} setSearchedPhrase={setSearchedPhrase} />
                     </Col>
                 </Row>
