b73743dbd4a69a3a694c5a31a482c4f211fb4da0
Paweł Stępień <finestedm@gmail.com>
2022-12-28 21:24:07 +0100

    small progress - sharing commit

--- src/components/ActiveFilterIndicator.js
+++ src/components/ActiveFilterIndicator.js
@@ -3,10 +3,17 @@ import { users } from '../data/users'
 
 export default function AcitveFiltersIndicator({ filters }) {
     
-    if (!(Object.values(filters.status)).every(val => val === true) || filters.users.length !== users.length) {
-        return(
-            <div>Tu pojawi się info o filtrach</div>
-        )
-    }
+    (Object.keys(filters.status)).map(stat => console.log(filters.status[stat]))
+
+    return(
+        (!(Object.keys(filters.status)).every(val => val === true)) &&
+        <div>{((Object.keys(filters.status)).filter(key => filters.status[key] === true))}</div>
+    )
+
+    // if (!(Object.values(filters.status)).every(val => val === true) || filters.users.length !== users.length) {
+    //     return(
+    //         (!(Object.values(filters.status)).every(val => val === true)) && (Object.values(filters.status)).map(stat => <div>{filters.status[stat]}</div>)
+    //     )
+    // }
 
 }
