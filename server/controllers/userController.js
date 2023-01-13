a12e0358db20713c8b1cd530f920fbc5d0772ecb
Paweł Stępień <finestedm@gmail.com>
2023-01-13 11:26:46 +0100

    getMe and Login wotking

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,9 +77,15 @@ const loginUser= asyncHandler(async (req, res) => {
 
 // @desc Get a user data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: 'User data display'})
+    const { _id, name, email } = await User.findById(req.user.id);
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
 function generateToken(id) {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import {registerUser, loginUser, getMe} from "../controllers/userController.js";
 import express from 'express';
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 export default router
