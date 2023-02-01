4494535349b38034e2d608dffd132b0602ae3188
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2023-02-01 08:16:07 +0000

    show navbar only if user logged in

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Archive from "./Pages/Archive";
 import Main from "./Pages/Main";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const { user } = useSelector((state) => state.authSlice)
+
+  console.log(window.location.pathname)
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL} className='App'>
       <Container fluid className='app-container h-100 d-flex flex-column flex-md-row align-items-stretch p-0 m-0'>
-        <Navbar className='topbar' />
-        {/* {window.location.pathname !== '/jungheinrich-project-number-generator/login' && <Navbar className='topbar' />} */}
+        {user && <Navbar className='topbar' />}
         <Routes>
           <Route path='/login' element={<Login />}/>
           <Route path='/signup' element={<SignUp />}/>
